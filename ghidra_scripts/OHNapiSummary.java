4c8c567ba4732857f794b97339c613fb525fcaf4
Parzival <2833456550@qq.com>
2024-10-21 19:22:59 +0800

    更新了获取注册函数的方式

--- ghidra_scripts/OHNapiSummary.java
+++ ghidra_scripts/OHNapiSummary.java
@@ -1,11 +1,15 @@
 import com.bai.util.*;
+import ghidra.program.model.address.Address;
 import ghidra.program.model.listing.Function;
+import ghidra.program.model.listing.FunctionManager;
+import ghidra.program.model.mem.MemoryAccessException;
+import ghidra.program.model.mem.MemoryBlock;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OHNapiSummary extends BinAbsInspector {
-
 
 
     @Override
@@ -30,26 +34,51 @@ public class OHNapiSummary extends BinAbsInspector {
 //        new EnvSetup(getCurrentProgram(), this, getState(), this).run();
 
 
-        List<Function> napi_registers = this.getGlobalFunctions("RegisterEntryModule");
-        if (napi_registers.size() != 1) {
-            this.println("[ERROR] cannot find unique function RegisterEntryModule");
-            if (napi_registers.isEmpty()) {
-                this.println("[ERROR] skip function RegisterEntryModule");
-            }
+        List<Function> registerFunctions = getRegisterFunctionAddress();
+
+        for (Function f : registerFunctions) {
+            printFunctionInfo(f);
         }
-        Function f = napi_registers.get(0);
-
-        System.out.println("Found "+napi_registers.size()+" RegisterEntryModule functions.");
-
-        // output some information about this function、
-        println("Function name: "+f.getName());
-        println("Function address: "+f.getEntryPoint());
-        println("Function signature: "+f.getSignature());
-        println("Function comment: "+f.getComment());
-
 
         long duration = System.currentTimeMillis() - start;
 
-        println("OHNapiSummary script execution time: "+duration + "ms.");
+        println("OHNapiSummary script execution time: " + duration + "ms.");
+    }
+
+    private List<Function> getRegisterFunctionAddress() throws MemoryAccessException {
+        FunctionManager functionManager = currentProgram.getFunctionManager();
+        MemoryBlock initArrayBlock = currentProgram.getMemory().getBlock(".init_array");
+
+        if (initArrayBlock == null) {
+            println("No .init_array section found.");
+            return null;
+        }
+
+        Address start = initArrayBlock.getStart();
+        Address end = initArrayBlock.getEnd();
+        List<Function> candidates = new ArrayList<>();
+
+        println("Identifying functions in .init_array section:");
+
+        for (Address addr = start; addr.compareTo(end) < 0; addr = addr.add(currentProgram.getDefaultPointerSize())) {
+            Address funcAddr = currentProgram.getAddressFactory().getDefaultAddressSpace().getAddress(currentProgram.getMemory().getLong(addr));
+            Function func = functionManager.getFunctionAt(funcAddr);
+            if (func != null) {
+                candidates.add(func);
+                println("Found function: " + func.getName() + " at address: " + func.getEntryPoint());
+            }
+        }
+
+        // 检查函数名是否符合 Register.*Entry，仅保留符合的函数
+        candidates.removeIf(func -> !func.getName().matches("Register.*Entry"));
+
+        return candidates;
+    }
+
+    private void printFunctionInfo(Function f) {
+        println("Function name: " + f.getName());
+        println("Function address: " + f.getEntryPoint());
+        println("Function signature: " + f.getSignature());
+        println("Function comment: " + f.getComment());
     }
 }
