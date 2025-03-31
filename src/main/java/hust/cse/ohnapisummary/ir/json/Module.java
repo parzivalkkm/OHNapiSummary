c579fb5f04713e2cffea3875780935e59629ee00
Parzival <2833456550@qq.com>
2025-03-31 21:06:55 +0800

    添加了未来可能需要的hap名称及so名称字段

--- src/main/java/hust/cse/ohnapisummary/ir/json/Module.java
+++ src/main/java/hust/cse/ohnapisummary/ir/json/Module.java
@@ -5,6 +5,12 @@ import java.util.List;
 import com.google.gson.annotations.SerializedName;
 
 public class Module {
+    @SerializedName("hap_name")
+    public String hapName = "@TEST_HAP_NAME";
+
+    @SerializedName("so_name")
+    public String soName = "@TEST_SO_NAME";
+
     @SerializedName("module_name")
     public String moduleName;
 
