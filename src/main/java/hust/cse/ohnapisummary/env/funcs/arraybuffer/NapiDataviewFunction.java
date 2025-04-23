858d1b2787efc9309a083ce6c34e3415be660b41
Parzival <2833456550@qq.com>
2025-04-23 21:17:12 +0800

    修改部分函数建模

--- src/main/java/hust/cse/ohnapisummary/env/funcs/arraybuffer/NapiDataviewFunction.java
+++ src/main/java/hust/cse/ohnapisummary/env/funcs/arraybuffer/NapiDataviewFunction.java
@@ -1,4 +1,4 @@
-package hust.cse.ohnapisummary.env.funcs.arrays;
+package hust.cse.ohnapisummary.env.funcs.arraybuffer;
 
 import com.bai.env.*;
 import ghidra.program.model.listing.Function;
@@ -12,30 +12,27 @@ import hust.cse.ohnapisummary.util.TypeCategory;
 import java.util.List;
 import java.util.Set;
 
-public class NapiTypedArrayFunction extends NAPIFunctionBase {
-    public NapiTypedArrayFunction() {
+public class NapiDataviewFunction extends NAPIFunctionBase {
+    public NapiDataviewFunction() {
         super(Set.of(
 
-            //NAPI_EXTERN napi_status napi_is_typedarray(napi_env env,
-            //                                           napi_value value,
-            //                                           bool* result);
-            "napi_is_typedarray",
-            //NAPI_EXTERN napi_status napi_create_typedarray(napi_env env,
-            //                                               napi_typedarray_type type,
-            //                                               size_t length,
-            //                                               napi_value arraybuffer,
-            //                                               size_t byte_offset,
-            //                                               napi_value* result);
-            "napi_create_typedarray",
-            //NAPI_EXTERN napi_status napi_get_typedarray_info(napi_env env,
-            //                                                 napi_value typedarray,
-            //                                                 napi_typedarray_type* type,
-            //                                                 size_t* length,
-            //                                                 void** data,
-            //                                                 napi_value* arraybuffer,
-            //                                                 size_t* byte_offset);
-
-            "napi_get_typedarray_info"
+            //NAPI_EXTERN napi_status napi_create_dataview(napi_env env,
+            //                                             size_t length,
+            //                                             napi_value arraybuffer,
+            //                                             size_t byte_offset,
+            //                                             napi_value* result);
+                "napi_create_dataview",
+            //NAPI_EXTERN napi_status napi_is_dataview(napi_env env,
+            //                                         napi_value value,
+            //                                         bool* result);
+                "napi_is_dataview",
+            //NAPI_EXTERN napi_status napi_get_dataview_info(napi_env env,
+            //                                               napi_value dataview,
+            //                                               size_t* bytelength,
+            //                                               void** data,
+            //                                               napi_value* arraybuffer,
+            //                                               size_t* byte_offset);
+                "napi_get_dataview_info"
         ));
     }
 
@@ -48,10 +45,10 @@ public class NapiTypedArrayFunction extends NAPIFunctionBase {
         KSet retKset = NAPIValueManager.getKSetForValue(TypeCategory.NAPI_STATUS, calleeFunc.getEntryPoint(), callNV, retALoc.getLen()* 8, calleeFunc, context, inOutEnv);
         inOutEnv.set(retALoc, retKset, true);
 
-        if(calleeFunc.getName().equals("napi_create_typedarray")) {
+        if(calleeFunc.getName().equals("napi_create_dataview")) {
 
-            List<ALoc> alocs = getParamALocs(calleeFunc, 5, inOutEnv);
-            NAPIValue localNV = recordLocal(context, calleeFunc, 5);
+            List<ALoc> alocs = getParamALocs(calleeFunc, 4, inOutEnv);
+            NAPIValue localNV = recordLocal(context, calleeFunc, 4);
             KSet kSetForValue = NAPIValueManager.getKSetForValue(TypeCategory.NAPI_VALUE, calleeFunc.getEntryPoint(), localNV, MyGlobalState.defaultPointerSize* 8, calleeFunc, context, inOutEnv);
             for (ALoc loc: alocs) {
                 KSet ks = inOutEnv.get(loc);
@@ -60,7 +57,7 @@ public class NapiTypedArrayFunction extends NAPIFunctionBase {
                     inOutEnv.set(ptr, kSetForValue, true); // *ptr = env
                 }
             }
-        } else if(calleeFunc.getName().equals("napi_is_typedarray")) {
+        } else if(calleeFunc.getName().equals("napi_is_dataview")) {
             // 向result中插入一个抽象值
             List<ALoc> alocs = getParamALocs(calleeFunc, 2, inOutEnv);
             // 记录这个返回值
@@ -75,21 +72,20 @@ public class NapiTypedArrayFunction extends NAPIFunctionBase {
                 }
             }
 
-        } else if(calleeFunc.getName().equals("napi_get_typedarray_info")) {
-            // TODO 这里 3 4 5 6 都是返回值
-            //NAPI_EXTERN napi_status napi_get_typedarray_info(napi_env env,
-            //                                                 napi_value typedarray,
-            //                                                 napi_typedarray_type* type,
-            //                                                 size_t* length,
-            //                                                 void** data,
-            //                                                 napi_value* arraybuffer,
-            //                                                 size_t* byte_offset);
+        } else if(calleeFunc.getName().equals("napi_get_dataview_info")) {
+            // TODO 这里 2 3 4 5 都是返回值
+            //NAPI_EXTERN napi_status napi_get_dataview_info(napi_env env,
+            //                                               napi_value dataview,
+            //                                               size_t* bytelength,
+            //                                               void** data,
+            //                                               napi_value* arraybuffer,
+            //                                               size_t* byte_offset);
 
 
 
-            List<ALoc> alocs = getParamALocs(calleeFunc, 3, inOutEnv);
+            List<ALoc> alocs = getParamALocs(calleeFunc, 2, inOutEnv);
             // 记录这个返回值
-            NAPIValue localNV = recordLocal(context, calleeFunc, 3);
+            NAPIValue localNV = recordLocal(context, calleeFunc, 2);
             // 向分析中写入一个抽象值
             KSet kSetForValue = NAPIValueManager.getKSetForValue(TypeCategory.NUMBER, calleeFunc.getEntryPoint(), localNV, MyGlobalState.defaultPointerSize * 8, calleeFunc, context, inOutEnv);
             for (ALoc loc : alocs) {
@@ -102,9 +98,9 @@ public class NapiTypedArrayFunction extends NAPIFunctionBase {
 
             // TODO data
 
-            alocs = getParamALocs(calleeFunc, 5, inOutEnv);
+            alocs = getParamALocs(calleeFunc, 4, inOutEnv);
             // 记录这个返回值
-            localNV = recordLocal(context, calleeFunc, 5);
+            localNV = recordLocal(context, calleeFunc, 4);
             // 向分析中写入一个抽象值
             kSetForValue = NAPIValueManager.getKSetForValue(TypeCategory.NAPI_VALUE, calleeFunc.getEntryPoint(), localNV, MyGlobalState.defaultPointerSize * 8, calleeFunc, context, inOutEnv);
             for (ALoc loc : alocs) {
@@ -115,10 +111,9 @@ public class NapiTypedArrayFunction extends NAPIFunctionBase {
                 }
             }
 
-            // 向result中插入一个抽象值
-            alocs = getParamALocs(calleeFunc, 6, inOutEnv);
+            alocs = getParamALocs(calleeFunc, 5, inOutEnv);
             // 记录这个返回值
-            localNV = recordLocal(context, calleeFunc, 6);
+            localNV = recordLocal(context, calleeFunc, 5);
             // 向分析中写入一个抽象值
             kSetForValue = NAPIValueManager.getKSetForValue(TypeCategory.NUMBER, calleeFunc.getEntryPoint(), localNV, MyGlobalState.defaultPointerSize * 8, calleeFunc, context, inOutEnv);
             for (ALoc loc : alocs) {
